eb2ee22d9775e0407352199f60dbcb26c92a2635
Md Sabbir Roshid Shohan <sabbirshohan80@gmail.com>
2024-07-13 03:45:11 +0600

    Complete

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,11 @@ import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
     <>
-      {/* <GlobalStyle />
+      <GlobalStyle />
       <Header />
       <Main />
-      <Footer /> */}
-      <h1>Hello Shohan</h1>
+      <Footer />
+      
     </>
   );
 }
